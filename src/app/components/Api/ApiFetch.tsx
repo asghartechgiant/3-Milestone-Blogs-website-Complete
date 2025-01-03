7f3b1c2c8b380c6286f73e4914c06959d747178c
Asghar Akram <asghar.akram004@gmail.com>
2025-01-03 15:56:48 +0500

    fsdfs

---
+++ src/app/components/Api/ApiFetch.tsx
@@ -0,0 +1,6 @@
+// export default async function ApiFetch() {
+//   const responce = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   const data = await responce.json();
+//   console.log(data);
+//   return <h1>hbhhb</h1>;
+// }
---
+++ src/sanity/schemaTypes/postType.ts
@@ -0,0 +1,70 @@
+import { DocumentTextIcon } from "@sanity/icons";
+import { defineArrayMember, defineField, defineType } from "sanity";
+
+export const postType = defineType({
+  name: "post",
+  title: "Post",
+  type: "document",
+  icon: DocumentTextIcon,
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    }),
+    defineField({
+      name: "author",
+      type: "reference",
+      to: { type: "author" },
+    }),
+    defineField({
+      name: "mainImage",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+        },
+      ],
+    }),
+    defineField({
+      name: "categories",
+      type: "array",
+      of: [defineArrayMember({ type: "reference", to: { type: "category" } })],
+    }),
+    defineField({
+      name: "publishedAt",
+      type: "datetime",
+    }),
+    // Adding the description field
+    defineField({
+      name: "description",
+      type: "text", // Using 'text' type for longer content
+      title: "Description",
+      description:
+        "A short description of the post for previews and search engines.",
+      rows: 5, // You can adjust the number of rows in the textarea
+    }),
+  ],
+  preview: {
+    select: {
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return { ...selection, subtitle: author && `by ${author}` };
+    },
+  },
+});
